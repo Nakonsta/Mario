bdc3d42be078c00c9d134c6435f20294a415ba4c
Anastasiia <na.konsta@gmail.com>
2024-06-12 11:45:49 +0200

    fix: move to vue

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppCanvas from './components/AppCanvas.vue';
+</script>
+
+<template>
+  <AppCanvas />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppCanvas.vue
@@ -0,0 +1,246 @@
+<script setup lang="ts">
+// @ts-nocheck
+import platformImgPath from '/img/platform.png';
+import backgroundImgPath from '/img/background.png';
+import hillsImgPath from '/img/hills.png';
+import { onMounted } from 'vue';
+
+const init = () => {
+  const canvas = document.querySelector('canvas');
+  const c = canvas.getContext('2d');
+
+  canvas.width = 1024;
+  canvas.height = 576;
+
+  const gravity = 1.5;
+
+  class Player {
+    constructor() {
+      this.position = {
+        x: 100,
+        y: 100,
+      };
+      this.velocity = {
+        x: 0,
+        y: 0,
+      };
+      this.width = 30;
+      this.height = 30;
+    }
+
+    draw() {
+      c.fillStyle = 'crimson';
+      c.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+
+    update() {
+      this.draw();
+
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+
+      if (this.position.y + this.height + this.velocity.y <= canvas.height)
+        this.velocity.y += gravity;
+    }
+  }
+
+  class Platform {
+    constructor({ x, y, width, height, image }) {
+      this.position = {
+        x,
+        y,
+      };
+      this.image = image;
+      this.width = width;
+      this.height = height;
+    }
+
+    draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }
+
+  class GenericObject {
+    constructor({ x, y, width, height, image }) {
+      this.position = {
+        x,
+        y,
+      };
+      this.image = image;
+      this.width = width;
+      this.height = height;
+    }
+
+    draw() {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }
+
+  function createImage(imagePath) {
+    const image = new Image();
+    image.src = imagePath;
+
+    return image;
+  }
+
+  const player = new Player();
+  const platforms = [
+    new Platform({
+      x: -1,
+      y: 470,
+      width: 580,
+      height: 20,
+      image: createImage(platformImgPath),
+    }),
+    new Platform({
+      x: 577,
+      y: 470,
+      width: 580,
+      height: 20,
+      image: createImage(platformImgPath),
+    }),
+    new Platform({
+      x: 1257,
+      y: 470,
+      width: 580,
+      height: 20,
+      image: createImage(platformImgPath),
+    }),
+  ];
+  const genericObjects = [
+    new GenericObject({
+      x: -1,
+      y: -1,
+      width: 11643,
+      height: 732,
+      image: createImage(backgroundImgPath),
+    }),
+    new GenericObject({
+      x: 0,
+      y: 0,
+      width: 7545,
+      height: 400,
+      image: createImage(hillsImgPath),
+    }),
+  ];
+  const keys = {
+    right: {
+      pressed: false,
+    },
+    left: {
+      pressed: false,
+    },
+  };
+
+  let scrollOffset = 0;
+
+  function animate() {
+    requestAnimationFrame(animate);
+    c.fillStyle = 'white';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    genericObjects.forEach((genericObject) => {
+      genericObject.draw();
+    });
+
+    platforms.forEach((platform) => {
+      platform.draw();
+    });
+
+    player.update();
+
+    if (keys.right.pressed && player.position.x < 400) {
+      player.velocity.x = 5;
+    } else if (keys.left.pressed && player.position.x > 100) {
+      player.velocity.x = -5;
+    } else {
+      player.velocity.x = 0;
+
+      if (keys.right.pressed) {
+        scrollOffset += 5;
+        platforms.forEach((platform) => {
+          platform.position.x -= 5;
+        });
+        genericObjects.forEach((genericObject) => {
+          genericObject.position.x -= 3;
+        });
+      } else if (keys.left.pressed) {
+        scrollOffset -= 5;
+        platforms.forEach((platform) => {
+          platform.position.x += 5;
+        });
+        genericObjects.forEach((genericObject) => {
+          genericObject.position.x += 3;
+        });
+      }
+    }
+
+    // platform collision detection
+    platforms.forEach((platform) => {
+      if (
+        player.position.y + player.height <= platform.position.y &&
+        player.position.y + player.height + player.velocity.y >=
+          platform.position.y &&
+        player.position.x + player.width >= platform.position.x &&
+        player.position.x <= platform.position.x + platform.width
+      ) {
+        player.velocity.y = 0;
+      }
+    });
+
+    if (scrollOffset >= 2000) {
+      console.log('win');
+    }
+  }
+
+  animate();
+
+  addEventListener('keydown', ({ keyCode }) => {
+    switch (keyCode) {
+      case 65:
+      case 37:
+        keys.left.pressed = true;
+        break;
+      case 83:
+      case 40:
+        break;
+      case 68:
+      case 39:
+        keys.right.pressed = true;
+        break;
+      case 87:
+      case 38:
+        player.velocity.y -= 20;
+        break;
+    }
+  });
+
+  addEventListener('keyup', ({ keyCode }) => {
+    switch (keyCode) {
+      case 65:
+      case 37:
+        keys.left.pressed = false;
+        break;
+      case 83:
+      case 40:
+        break;
+      case 68:
+      case 39:
+        keys.right.pressed = false;
+        break;
+      case 87:
+      case 38:
+        player.velocity.y -= 20;
+        break;
+    }
+  });
+};
+
+onMounted(init);
+</script>
+
+<template>
+  <canvas></canvas>
+</template>
+
+<style scoped></style>
